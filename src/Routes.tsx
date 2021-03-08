6125b36689534da292e1b0c9aa35576d9dd6deec
handikoswara24 <39146228+handikoswara24@users.noreply.github.com>
2021-03-08 20:28:31 +0700

    Route Parameter

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -4,13 +4,15 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./Header"
 import AdminPage from "./AdminPage";
 import ProductsPage from "./ProductsPage";
+import ProductPage from "./ProductPage";
 
 const Routes: React.SFC = () => {
     return (
         <Router>
             <div>
                 <Header></Header>
-                <Route path="/products" component={ProductsPage}></Route>
+                <Route exact={true} path="/products" component={ProductsPage}></Route>
+                <Route path="/products/:id" component={ProductPage}></Route>
                 <Route path="/admin" component={AdminPage}></Route>
             </div>
         </Router>
