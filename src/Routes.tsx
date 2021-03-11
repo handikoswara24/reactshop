82ffb80f11bad9045a7721aedd0d0407846537ac
handikoswara24 <39146228+handikoswara24@users.noreply.github.com>
2021-03-11 17:22:11 +0700

    Not Found Page

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,19 +1,23 @@
 import * as React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Header from "./Header"
 import AdminPage from "./AdminPage";
 import ProductsPage from "./ProductsPage";
 import ProductPage from "./ProductPage";
+import NotFoundPage from "./NotFoundPage";
 
 const Routes: React.SFC = () => {
     return (
         <Router>
             <div>
                 <Header></Header>
-                <Route exact={true} path="/products" component={ProductsPage}></Route>
-                <Route path="/products/:id" component={ProductPage}></Route>
-                <Route path="/admin" component={AdminPage}></Route>
+                <Switch>
+                    <Route exact={true} path="/products" component={ProductsPage}></Route>
+                    <Route path="/products/:id" component={ProductPage}></Route>
+                    <Route path="/admin" component={AdminPage}></Route>
+                    <Route component={NotFoundPage}></Route>
+                </Switch>
             </div>
         </Router>
     )
