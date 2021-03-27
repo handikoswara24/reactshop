0303f0917e5f42d6979e6ece94edfa0cccdcd286
handikoswara24 <39146228+handikoswara24@users.noreply.github.com>
2021-03-27 18:20:10 +0700

    Lazy Loading Route

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,12 +1,15 @@
 import * as React from "react";
+import { Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch, Redirect, RouteComponentProps } from "react-router-dom";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import Header from "./Header"
-import AdminPage from "./AdminPage";
+
 import ProductsPage from "./ProductsPage";
 import ProductPage from "./ProductPage";
 import NotFoundPage from "./NotFoundPage";
 import LoginPage from "./LoginPage";
+
+const AdminPage = React.lazy(() => import("./AdminPage"));
 
 const RoutesWrap = () => {
     return (
@@ -39,7 +42,9 @@ class Routes extends React.Component<RouteComponentProps, IState> {
                             <Route exact={true} path="/products" component={ProductsPage}></Route>
                             <Route path="/products/:id" component={ProductPage}></Route>
                             <Route path="/admin">
-                                {this.state.loggedIn ? <AdminPage /> : <Redirect to="/login"></Redirect>}
+                                {this.state.loggedIn ? (
+                                    <Suspense fallback={<div className="page-container">Loading...</div>}><AdminPage /></Suspense>)
+                                    : <Redirect to="/login"></Redirect>}
                             </Route>
                             <Route path="/login" component={LoginPage}></Route>
                             <Route component={NotFoundPage}></Route>
