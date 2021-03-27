66a3b95fd92e8aab92f02a17ecdfe29f1fb826f0
handikoswara24 <39146228+handikoswara24@users.noreply.github.com>
2021-03-27 18:01:41 +0700

    Animated Transition

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
-import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
-
+import { BrowserRouter as Router, Route, Switch, Redirect, RouteComponentProps } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import Header from "./Header"
 import AdminPage from "./AdminPage";
 import ProductsPage from "./ProductsPage";
@@ -8,26 +8,50 @@ import ProductPage from "./ProductPage";
 import NotFoundPage from "./NotFoundPage";
 import LoginPage from "./LoginPage";
 
-const Routes: React.SFC = () => {
-    // eslint-disable-next-line
-    const [loggedIn, setLoggedIn] = React.useState(true);
+const RoutesWrap = () => {
     return (
         <Router>
-            <div>
-                <Header></Header>
-                <Switch>
-                    <Redirect exact={true} from="/" to="products"></Redirect>
-                    <Route exact={true} path="/products" component={ProductsPage}></Route>
-                    <Route path="/products/:id" component={ProductPage}></Route>
-                    <Route path="/admin">
-                        {loggedIn ? <AdminPage /> : <Redirect to="/login"></Redirect>}
-                    </Route>
-                    <Route path="/login" component={LoginPage}></Route>
-                    <Route component={NotFoundPage}></Route>
-                </Switch>
-            </div>
+            <Route component={Routes}></Route>
         </Router>
-    )
+    );
+};
+
+interface IState {
+    loggedIn: boolean
 }
 
-export default Routes;
+class Routes extends React.Component<RouteComponentProps, IState> {
+    public constructor(props: RouteComponentProps) {
+        super(props);
+        this.state = {
+            loggedIn: true
+        }
+    }
+    public render() {
+        return (
+
+            <div>
+                <Header></Header>
+                <TransitionGroup>
+                    <CSSTransition key={this.props.location.key} timeout={500} classNames="animate">
+                        <Switch>
+                            <Redirect exact={true} from="/" to="products"></Redirect>
+                            <Route exact={true} path="/products" component={ProductsPage}></Route>
+                            <Route path="/products/:id" component={ProductPage}></Route>
+                            <Route path="/admin">
+                                {this.state.loggedIn ? <AdminPage /> : <Redirect to="/login"></Redirect>}
+                            </Route>
+                            <Route path="/login" component={LoginPage}></Route>
+                            <Route component={NotFoundPage}></Route>
+                        </Switch>
+                    </CSSTransition>
+                </TransitionGroup>
+
+            </div>
+
+        )
+    }
+
+}
+
+export default RoutesWrap;
